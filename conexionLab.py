fba447137b279c4933018ba9a33b93542037921f
wendyvillegas <wendy.villegas.p@outlook.com>
2026-03-26 23:46:42 -0500

    Update conexionLab.py

--- conexionLab.py
+++ conexionLab.py
@@ -20,3 +20,9 @@ for node in lab.nodes():
     print("Console key:", console_key)
     console_url = f"telnet 10.10.20.40 {console_key}"
     print("Comando consola:", console_url)
+
+
+##sudo ip addr add 192.168.20.10/24 dev eth0
+
+
+#sudo ip route add default via 192.168.20.1 dev eth0
